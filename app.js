423767d8214ca927883914f7972e1b5ea0c1e55a
MateusCDev <mateus.cesararaujo@hotmail.com>
2022-07-02 19:57:34 -0300

    Adicinando exemplos e organizando a pasta

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 "use strict";
-let button = document.getElementById('button');
-let input1 = document.getElementById('input1');
-let input2 = document.getElementById('input2');
-function somaNumero(num1, num2) {
-    return num1 + num2;
+const pessoa = {
+    nome: 'Mateus',
+    idade: 23,
+    profissao: 'desenvolvedor'
+};
+pessoa.idade = 28;
+const andre = {
+    nome: "Andre",
+    idade: 25,
+    profissao: "pintor"
+};
+const paula = {
+    nome: "João",
+    idade: 30,
+    profissao: "desenvolvedor"
+};
+var Profissao;
+(function (Profissao) {
+    Profissao[Profissao["Professora"] = 0] = "Professora";
+    Profissao[Profissao["Atriz"] = 1] = "Atriz";
+    Profissao[Profissao["Desenvolvedor"] = 2] = "Desenvolvedor";
+    Profissao[Profissao["JogadorDeFutebol"] = 3] = "JogadorDeFutebol";
+})(Profissao || (Profissao = {}));
+const vanessa = {
+    nome: 'Vanessa',
+    idade: 23,
+    profissao: Profissao.Atriz
+};
+const maria = {
+    nome: 'Maria',
+    idade: 23,
+};
+const jessica = {
+    nome: 'Jessica',
+    idade: 28,
+    profissao: Profissao.Professora,
+    materias: ['Matematica', 'Contabilidade']
+};
+function listar(lista) {
+    for (const item of lista) {
+        console.log('- ', item);
+    }
 }
-if (button) {
-    button.addEventListener('click', () => {
-        if (input1 && input2) {
-            console.log(somaNumero(Number(input1.value), Number(input2.value)));
-        }
-    });
-}
+listar(jessica.materias);
--- app.ts
+++ app.ts
@@ -1,14 +1,63 @@
-let button = document.getElementById('button');
-let input1 = document.getElementById('input1') as HTMLInputElement;
-let input2 = document.getElementById('input2') as HTMLInputElement;
+const pessoa ={
+    nome:'Mateus',
+    idade: 23,
+    profissao: 'desenvolvedor'
+}
 
-function somaNumero (num1:number,num2:number){
-    return num1 + num2;
+pessoa.idade = 28;
+
+const andre: {nome:string, idade:number, profissao:string} = {
+    nome: "Andre",
+    idade: 25,
+    profissao: "pintor"
 }
-if(button){
-    button.addEventListener('click',()=>{
-        if(input1 && input2){
-            console.log(somaNumero(Number(input1.value), Number(input2.value))); 
-        }
-    })
+
+const paula: {nome:string, idade:number, profissao:string} = {
+    nome: "João",
+    idade: 30,
+    profissao: "desenvolvedor"
 }
+
+enum Profissao{
+    Professora,
+    Atriz,
+    Desenvolvedor,
+    JogadorDeFutebol
+}
+
+interface Pessoa{
+    nome: string,
+    idade: number,
+    profissao?: Profissao
+}
+
+interface Estudante extends Pessoa{
+    materias: string[]
+}
+
+const vanessa : Pessoa ={
+    nome: 'Vanessa',
+    idade: 23,
+    profissao: Profissao.Atriz
+}
+
+const maria : Pessoa ={
+    nome: 'Maria',
+    idade: 23,
+}
+
+const jessica: Estudante = {
+    nome: 'Jessica',
+    idade: 28,
+    profissao: Profissao.Professora,
+    materias: ['Matematica', 'Contabilidade']
+}
+
+function  listar(lista: string[]){
+    for (const item of lista){
+        console.log('- ', item)
+    }
+}
+
+
+listar(jessica.materias);
